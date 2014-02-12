dbfe50487d4ebcb04ecbea10862c19fbee5c07cd
rap2hpoutre <raphaelht@gmail.com>
2014-02-12 14:22:19 +0100

    PSR-4 + gitignore

--- app/TableSchema.class.php
+++
@@ -1,2 +0,0 @@
-<?php
-namespace Rap2hpoutre\MySQLExplainExplain;
--- app/base.php
+++ app/base.php
@@ -1,28 +1,65 @@
 <?php
+// Composer install verification : https://github.com/rap2hpoutre/mysql-xplain-xplain/issues/4
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) exit('Install dependencies : "composer install"');
 
-require '../vendor/autoload.php';
+/**
+ * Autoloader PSR-4 (bit.ly/1fg1P08)
+ */
+spl_autoload_register(function ($class) {
 
-require '../app/Explainer.class.php';
-require '../app/Row.class.php';
-require '../app/Cell.class.php';
-require '../app/Key.class.php';
-require '../app/Column.class.php';
-require '../app/constants.php';
+	// project-specific namespace prefix
+	$prefix = 'Rap2hpoutre\\MySQLExplainExplain\\';
 
+	// base directory for the namespace prefix
+	$base_dir = __DIR__ . '/';
+
+	// does the class use the namespace prefix?
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+		// no, move to the next registered autoloader
+		return;
+	}
+
+	// get the relative class name
+	$relative_class = substr($class, $len);
+
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+	// if the file exists, require it
+	if (file_exists($file)) {
+	    require $file;
+	}
+});
+
+// Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+// Some consts
+require __DIR__ . '/constants.php';
+
+// Session
 session_start();
 
-// Connexion permanente
-if (file_exists('../conf/db.php')) {
-	require '../conf/db.php';
+// Permanent login
+if (file_exists(__DIR__ . '/../conf/db.php')) {
+	require __DIR__ . '/../conf/db.php';
 }
 
+// UTF-8
 header('Content-Type: text/html; charset=utf-8');
 
-$engine = new \League\Plates\Engine('../app/templates');
+// Template engine
+$engine = new \League\Plates\Engine(__DIR__ . '/templates');
+
+// Template
 $template = new \League\Plates\Template($engine);
 
+// Template title
 $template->title = "MySQL Explain Explain";
 
+// Flash message
 if (isset($_SESSION['flash_message'])) {
 	$template->flash_message  = $_SESSION['flash_message'];
 	unset($_SESSION['flash_message']);
